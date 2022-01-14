eb06ec0eb2400765ff58d193adc6b7ab58b70f9a
Daniel Shephard <daniel.b.shephard@gmail.com>
2022-01-15 01:25:47 +1100

    deploy-commands init

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const discord = require("discord.js");
-const client = new discord.Client();
+// Require the necessary discord.js classes
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json");
 
-client.on("ready", () => {
-  console.log("Connected to the bot");
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on("message", (msg) => {
-  if (msg.content === "hi") {
-    msg.reply("Hi to you too!");
-  }
-});
-
-client.login("OTMxNTQxNjEwNzA0MDExMzU0.YeF7uw.88UgoRXUJYDLTkvHaqxO2TPJ0LU");
+// Login to Discord with your client's token
+client.login(token);
